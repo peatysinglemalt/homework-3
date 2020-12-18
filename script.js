b777743a58cb24a89ff8177121db5a4c0564fae1
mateo's mbp <mateodubs@mateos-MacBook-Pro.local>
2020-12-18 08:18:59 -0800

    created prompts within js

--- script.js
+++ script.js
@@ -1,9 +1,86 @@
+// 1. click the button to generate a password
+// 2. prompts for password length 
+// 3. Enter a length of at least 8 characters and no more than 128 characters
+// 4. prompt for each of the following password combinations:
+      // Include lowercase characters in the password? 
+      // Include Uppercase characters in the password? 
+      // Include numeric characters in the password? 
+      // Include special characters in the password? 
+// WHEN I answer each prompt
+// THEN my input should be validated and at least one character type should be selected
+// WHEN all prompts are answered
+// THEN a password is generated that matches the selected criteria
+// WHEN the password is generated
+// THEN the password is either displayed in an alert or written to the page 
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+//1. prompts to alert useer input 
+function confirmlenght () {
+  length = 0; 
+
+  while (length < 8 || length > 128) {
+    var length = prompt("Choose between a minimun of 8 characters and no more than 128 characters."); 
+    if (length < 8 || length > 128) {
+      alert("Password length must be between 8 to 128 characters"); 
+    }
+  }
+  return length;
+}
+
+function confirmlowercase () {
+  var lowercase = confirm ("Include lowercase characters in the password?")
+  return lowercase; 
+}
+
+function confirmuppercase () {
+  var uppercase = confirm ("Include uppercase characters in the password?")
+  return uppercase; 
+}
+
+function confirmnumeric () {
+  var numeric = confirm ("Include numeric characters in the password?")
+  return numeric; 
+}
+
+function confirmspecial () {
+  var special = confirm ("Include special characters in the password?")
+  return special; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
+
   var password = generatePassword();
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
